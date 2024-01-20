eee6989a9432b91769585e10e53d4b9625c95394
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2024-01-21 01:54:35 +0200

    Added a border so it looks more like an envelope

--- invait/java.js
+++ invait/java.js
@@ -5,6 +5,8 @@ but.onclick = function open() {
     document.getElementById('envelopee').className = 'fa-regular fa-envelope-open'
     tekst.setAttribute("style", "display: flex; animation: fadein 1s;")
     document.getElementById('envelopee').setAttribute("style", "animation: down 1.5s;")
+    document.getElementById('tekstut').setAttribute("style", "display: flex; animation: fadein 1s;")
+    document.getElementById('tekstut').style.border = "3px solid black"
     setTimeout(() => {
         document.getElementById('envelopee').style.display = "none"
       }, 600);
